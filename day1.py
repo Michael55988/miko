c1b1232605d9f0ba173803e251bf8dcef9a012b8
MichaelFellous <michaelfellous2@gmail.com>
2025-03-10 13:33:13 +0200

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,50 @@
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+
+# print (('Hello world' + ' ') *4)
+
+# print((99**3)*8)
+
+
+# print(5 < 3)
+# print(3 == 3)
+# print(3 == '3')
+# print ('Hello' == 'hello')
+
+
+# computer_brand = 'Apple'
+# print(f'i have an {computer_brand} computer')
+
+# name = 'Michael'
+# age = '26'
+# shoe_size = '44'
+# info = f'Hello my name is {name} I am {age} years old when i play soccer i need size {shoe_size} shoes to be confortable'
+# print(f'{info}')
+
+
+# a = '55'
+# b = '33'
+# if a > b:
+#   print('Hello world')
+
+# user_num = int(input('choose a number:'))
+# if user_num %2 == 0:
+#     print(f'{user_num} is odd')
+# else:
+#      print(f'{user_num} is even')
+
+# my_name = 'Michael'
+# user_name = input('what is your name?:')
+# if user_name == my_name:
+#       print('Our moms are twins')
+# else:
+#       print('Who are you')
+
+
+user_height = int(input('what is your height?'))
+if user_height > 145:
+      print('you can ride')
+elif user_height < 145:
+      print('you must grow to ride my little boy!')
