1e60f82156aec2bbcf3cf456dcbb15982aa2e8de
MichaelFellous <michaelfellous2@gmail.com>
2025-03-20 15:22:23 +0200

    Add files via upload

---
+++ W2D1.py
@@ -0,0 +1,113 @@
+# exercices xp:
+
+
+# exercice 1:
+
+# def display_message():
+#     print('i am learning python')
+
+# display_message()
+
+
+
+
+
+
+# exercice 2:
+
+# def fav_book(title):
+#     print(f'one of my fav books is {title} ')
+
+
+
+# fav_book('Harry Potter')
+
+
+
+
+# exercice 3
+
+# def describe_city(city, country):
+#     print(f'{city} is in {country}')
+
+# describe_city('Jerusalem', 'Israel')
+# describe_city('Paris' , 'France')
+
+
+
+
+
+# exercice4
+
+# import random
+
+# def random_number():
+    
+#     num = random.randint(1, 100)
+#     print(f"Your random number is: {num}")
+#     num2 = random.randint(1, 100)
+#     print(f'your random number is {num2}')
+
+#     if num == num2:
+#         print('go buy a loto ticket')
+#     else:
+#         print(f'next time houya! {num},{num2}')
+
+# random_number()
+
+
+
+
+
+
+
+
+# exercice5
+
+# def make_shirt(size='Large', message="i love python" ):
+#    print(f'the size of the shirt is {size} and the text is {message}') 
+
+# make_shirt()
+# make_shirt('XL')
+# make_shirt('Medium')
+# make_shirt('Small', "ICI C'EST PARIS")
+# make_shirt(size='XSmall', message='tuto bene')
+
+
+
+
+
+
+
+
+
+
+# exercice6
+
+
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+# def show_magicians(magicians):
+    
+#     for magician in magicians:
+#         print(magician)
+
+# def make_great(magicians):
+   
+#     for i in range(len(magicians)):
+#         magicians[i] = magicians[i] + " the Great"
+
+
+# make_great(magician_names)
+
+
+# show_magicians(magician_names)
+
+
+
+
+
+
+# exercice7
+
+# i didnt had time to finish yet
